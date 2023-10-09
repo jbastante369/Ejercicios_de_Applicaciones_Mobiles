8aff895ac173ad33e015d3c3ddda0a4b0c2ccf85
Julian <jbastante369@gmail.com>
2023-10-09 18:37:49 +0200

    AÑado ventana2

--- VariousActivitys/app/build.gradle.kts
+++ VariousActivitys/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.variosactivitys_guardardatos"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.variosactivitys_guardardatos"
@@ -32,6 +32,9 @@ android {
     }
     kotlinOptions {
         jvmTarget = "1.8"
+    }
+    buildFeatures{
+        viewBinding=true
     }
 }
 
--- VariousActivitys/app/src/main/java/com/example/variosactivitys_guardardatos/MainActivity.kt
+++ VariousActivitys/app/src/main/java/com/example/variosactivitys_guardardatos/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.variosactivitys_guardardatos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.variosactivitys_guardardatos.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.boton.setOnClickListener {
+            IrAVentana2()
+        }
+    }
+
+    private fun IrAVentana2() {
+        var miIntent: Intent = Intent(this,Ventana2::class.java)
+        //miIntent.putExtra("nombre",binding.cajaNombre.text.toString())
+        //miIntent.putExtra("edad",binding.cajaEdad.text.toString())
+        startActivity(miIntent)
     }
 }
+
