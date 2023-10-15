8133c3e52a8f172bdb1cfbd642e881f12dfa7e82
Julian <jbastante369@gmail.com>
2023-10-15 12:01:13 +0200

    Commit2 Encuesta

---
+++ Encuesta/app/src/main/java/com/example/encuesta/Encuesta.kt
@@ -0,0 +1,5 @@
+package com.example.encuesta
+
+class Encuesta (nombre: String,sistema_operativo: String,especialidades:List<String>,horas_estudio:Int){
+
+}
--- Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
+++ Encuesta/app/src/main/java/com/example/encuesta/MainActivity.kt
@@ -2,10 +2,65 @@ package com.example.encuesta
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.encuesta.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=ActivityMainBinding.inflate(layoutInflater)
+
+        var listaEncuestas : List<Encuesta> = listOf()
+        var nombre : String
+        var sistema_operativo : String
+        var especialidades : List<String> = listOf()
+
+        binding.btnValidar.setOnClickListener {
+            if (binding.edtNombre.text.isBlank() && binding.swAnonimo.isActivated!!){
+                Toast.makeText(this,"Te falta rellenar el nombre o ponerlo como anonimo", Toast.LENGTH_SHORT).show()
+            }else if(binding.rdbMac.isChecked!! && binding.rdbWindows.isChecked!! && binding.rdbLinux.isChecked!!){
+                Toast.makeText(this,"Elija un sistema operativo", Toast.LENGTH_SHORT).show()
+            }else{
+                if(binding.swAnonimo.isChecked){
+                    nombre = "Animo"
+                }else{
+                    nombre = binding.edtNombre.text.toString()
+                }
+
+                if(binding.rdbMac.isChecked){
+                    sistema_operativo = "Mac"
+                }else if (binding.rdbWindows.isChecked){
+                    sistema_operativo = "Windows"
+                }else{
+                    sistema_operativo = "Linux"
+                }
+
+                if (binding.chbDAM.isChecked){
+                    especialidades += "DAM"
+                }
+                if (binding.chbASIR.isChecked){
+                    especialidades += "ASIR"
+                }
+                if (binding.chbDAW.isChecked){
+                    especialidades += "DAW"
+                }
+
+                var encuesta = Encuesta(nombre,sistema_operativo, especialidades,0)
+                listaEncuestas += encuesta
+            }
+        }
+
+        binding.btnCuantas.setOnClickListener{
+            Toast.makeText(this,"Hay "+ listaEncuestas.size + " encuestas", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.btnResumen.setOnClickListener {
+            var resumen : String = ""
+            for (i in listaEncuestas){
+                resumen += i.toString() + "\n"
+            }
+            binding.txtResumen.setText(resumen)
+        }
     }
 }
