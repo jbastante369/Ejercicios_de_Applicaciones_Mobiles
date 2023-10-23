caac9f606831991f4754ce0bdea0cd30434ac690
Julian <jbastante369@gmail.com>
2023-10-23 18:47:52 +0200

    Recycler Prueba

---
+++ VistaRecicla/app/src/main/java/com/example/vistarecicla/MainActivity2.kt
@@ -0,0 +1,23 @@
+package com.example.vistarecicla
+
+import Modelo.Personaje
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.vistarecicla.databinding.ActivityMain2Binding
+
+class MainActivity2 : AppCompatActivity() {
+    lateinit var binding: ActivityMain2Binding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var p  = intent.getSerializableExtra("obj") as Personaje
+        binding.txtPersona2.text = p.toString()
+
+        binding.btnVolver.setOnClickListener {
+            finish()
+        }
+    }
+}
